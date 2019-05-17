3d27512e4c9df092e451b84707a48ba83e9859a5
Tyler Leung <tyler@Tylers-MacBook-Pro.local>
2019-05-18 01:59:33 +0800

    first version of blockchain

---
+++ blockchain.go
@@ -0,0 +1,17 @@
+package main
+
+type Blockchain struct {
+	blocks []*Block
+}
+
+// AddBlock saves provided data as a block in the blockchain
+func (bc *Blockchain) AddBlock(data string) {
+	prevBlock := bc.blocks[len(bc.blocks)-1]
+	newBlock := NewBlock(data, prevBlock.Hash)
+	bc.blocks = append(bc.blocks, newBlock)
+}
+
+// NewBlockChain creates a new Blockchain with genesis Block
+func NewBlockchain() *Blockchain {
+	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
